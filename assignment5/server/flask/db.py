dde23e35258759126af25de7c77f35834e0a450d
shreyan25T <shreyan.shetty@tigeranalytics.com>
2022-10-18 22:31:30 +0530

    new change

--- assignment5/server/flask/db.py
+++ assignment5/server/flask/db.py
@@ -1,49 +1,19 @@
+import sqlite3
+from pathlib import Path
 
-import csv
-
+import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 
-engine = create_engine('sqlite:///data.db', echo=True)
+Path("data.db").touch()
 
-metadata = MetaData()
-# Define the table with sqlalchemy:
-petrol = Table('Petrol', metadata,
-    Column('id',Integer, primary_key=True),
-    Column('Country',String(100)),
-    Column('Daily Oil Consumption (Barrels)',Integer),
-    Column('World Share',String(20)),
-    Column('Yearly Gallons Per Capita',Integer),
-    Column('Price Per Gallon (USD)',Integer),
-    Column('Price Per Liter (USD)',Integer),
-    Column('Price Per Liter (PKR)',Integer),
-    Column('GDP Per Capita ( USD )',Integer),
-    Column('Gallons GDP Per Capita Can Buy',Integer),
-    Column('xTimes Yearly Gallons Per Capita Buy',Integer),
-    Column('World Share1',Integer),
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
 
-)
-metadata.create_all(engine)
-insert_query = petrol.insert()
+petrol_data = pd.read_csv("Petrol Dataset.csv")
 
+petrol_data.to_sql("petrol", connection, if_exists="replace", index=False)
 
-with open('Petrol.csv', 'r', encoding="utf-8") as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
-    engine.execute(
-        insert_query,
-        [{
-        "Country": row[0],
-        "Daily Oil Consumption (Barrels)": row[1],
-        "World Share": row[2],
-        "Yearly Gallons Per Capita": row[3],
-        "Price Per Gallon (USD)": row[4],
-        "Price Per Liter (USD)": row[5],
-        "Price Per Liter (PKR)": row[6],
-        "GDP Per Capita ( USD )": row[7],
-        "Gallons GDP Per Capita Can Buy": row[8],
-        "xTimes Yearly Gallons Per Capita Buy": row[9],
-        "World Share1": row[10]}
-            for row in csv_reader]
-    )
+connection.commit()
+connection.close()
--- assignment5/server/flask/resources/charts.py
+++ assignment5/server/flask/resources/charts.py
@@ -1,6 +1,4 @@
-
-import json
-
+import sqlite3
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -9,28 +7,29 @@ import plotly.express as px
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
+cnx = sqlite3.connect("data.db")
+df = pd.read_sql_query("SELECT * FROM petrol",cnx)
 
 class BarChartOne(Resource):
-    df=pd.read_csv('Petrol Dataset.csv')
     @jwt_required()
     def get(self):
-        res = self.df.groupby('Country')['Daily Oil Consumption (Barrels)'].mean().sort_values(ascending=False)
+        res = df.groupby('Country')['Daily Oil Consumption (Barrels)'].mean().sort_values(ascending=False)
         res = res.head(10)
         return {"x":list(res.index),"y":list(res.values)}
 
 class BarChartTwo(Resource):
-    df=pd.read_csv('Petrol Dataset.csv')
+
     @jwt_required()
     def get(self):
-        res = self.df.groupby('Country')['Price Per Liter (USD)'].mean().sort_values(ascending=False)
+        res = df.groupby('Country')['Price Per Liter (USD)'].mean().sort_values(ascending=False)
         res = res.head(10)
         return {"x":list(res.index),"y":list(res.values)}
 
 class HistogramChart(Resource):
-     df=pd.read_csv('Petrol Dataset.csv')
+
      @jwt_required()
      def get(self):
-         cp =np.array(self.df.select_dtypes("number").drop(columns=["World Share1", "Price Per Liter (PKR)"]).corr())
+         cp =np.array(df.select_dtypes("number").drop(columns=["World Share1", "Price Per Liter (PKR)"]).corr())
          print(cp)
          m,n = cp.shape
          R,C = np.mgrid[:m,:n]
@@ -40,10 +39,10 @@ class HistogramChart(Resource):
          return {"x":data}
 
 class ScatterChart(Resource):
-    df=pd.read_csv('Petrol Dataset.csv')
+
     @jwt_required()
     def get(self):
-        sc =np.array(self.df[['Daily Oil Consumption (Barrels)','Price Per Liter (USD)']])
+        sc =np.array(df[['Daily Oil Consumption (Barrels)','Price Per Liter (USD)']])
         data =sc.tolist()
         return {"x":data}
 
